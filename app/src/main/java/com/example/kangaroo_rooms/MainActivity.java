b137c5ffb2995578ed7a63ca51a448dc2c4671d3
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2019-06-05 02:52:55 +0530

    Creating animation

--- app/src/main/java/com/example/kangaroo_rooms/MainActivity.java
+++ app/src/main/java/com/example/kangaroo_rooms/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.kangaroo_rooms;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.daimajia.androidanimations.library.Techniques;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+public class MainActivity extends AwesomeSplash {
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+        //hide the actionBar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        //Setting Fullscreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //Background Animation
+        configSplash.setBackgroundColor(R.color.bg_color);
+        configSplash.setAnimCircularRevealDuration(3000);
+        configSplash.setRevealFlagX(Flags.REVEAL_LEFT);
+        configSplash.setRevealFlagX(Flags.REVEAL_BOTTOM);
+
+        //kangaroo
+        configSplash.setLogoSplash(R.drawable.kang);
+        configSplash.setAnimLogoSplashDuration(5000);
+        configSplash.setAnimLogoSplashTechnique(Techniques.BounceInRight);
+
+        //text
+        configSplash.setTitleSplash("Complete Rental Solution");
+        configSplash.setTitleTextColor(R.color.txt_color);
+        configSplash.setTitleTextSize(30f);
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void animationsFinished() {
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
     }
+
+//    Handler handler;
+
+//        // Adding a delay
+//        handler=new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+//                startActivity(intent);
+//            }
+//        },2000);
+
 }
+
